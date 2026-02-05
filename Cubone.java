426039e181ef5c840ca75fb4cc74c91b9c1ca4c4
SpoopyEd <eduardoaguilera1031@gmail.com>
2026-02-04 23:59:18 -0800

    Main built, Assignment5 file not running fixed

--- Cubone.java
+++ Cubone.java
@@ -5,8 +5,9 @@
 
 public class Cubone extends Pokemon {
     private final String species = "Cubone"; // Any cubone object will be the pokemon species cubone
-    public Cubone(String name, String type, int lvl) { // constructs Cubone using parameters
-        super(name , type, lvl);
+    private final String type = "Normal"; // all cubone pokemon are normal type
+    public Cubone(String name) { // constructs Cubone using parameters
+        super(name);
     }
     public void attack() { // overrides attack from animal class
         System.out.println("Wack!");
--- Pokemon.java
+++ Pokemon.java
@@ -5,14 +5,14 @@
 // there is no such things as a Pokemon, they have a species, so this is abstract and a pokemon object
 // Cannot be made, only extensions
 public abstract class Pokemon {
-    private String species; // maybe final
-    private String name;
-    private String type;
-    private int lvl;
-    public Pokemon() {
-
+    private String species;
+    private String name; // name is determined by user
+    private String type; // type is determined based on species
+    private int lvl; // random int with limits (1-10)
+    public Pokemon(String name) {
+        this.name = name;
     }
-    public Pokemon(String name, String type, int lvl) {
+    public Pokemon(String name, String type, int lvl) { // not sure if this constructor would ever be used
         this.name = name;
         this.type = type;
         this.lvl = lvl;
